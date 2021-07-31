aeb046f6dad19cf6064c667219a8aaa13921988f
Alex Watts <alexjwatts83@gmail.com>
2021-07-31 14:32:38 +1000

    initial commit

---
+++ src/DbUp.Application/Interfaces/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbUp.Application.Interfaces
+{
+    public interface IGenericRepository<TEntity, TKey> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(TKey id);
+        Task<IReadOnlyList<TEntity>> GetAllAsync();
+        Task<int> AddAsync(TEntity entity);
+        Task<int> UpdateAsync(TEntity entity);
+        Task<int> DeleteAsync(TKey id);
+    }
+}
---
+++ src/DbUp.Domain/Entities/Location.cs
@@ -0,0 +1,10 @@
+﻿namespace DbUp.Domain.Entities
+{
+    public class Location
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+    }
+}
