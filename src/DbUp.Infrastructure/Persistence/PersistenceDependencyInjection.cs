b0a35a953ac439306428ae7f457b18e8fbff7089
Alex Watts <alexjwatts83@gmail.com>
2021-07-31 14:48:00 +1000

    Added nuget packages

---
+++ src/DbUp.Infrastructure/Persistence/PersistenceDependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DbUp.Infrastructure.Persistence
+{
+    public static class PersistenceDependencyInjection
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<ConnectionStringSettings>(configuration.GetSection(ConnectionStringSettings.Section));
+
+            return services;
+        }
+    }
+}
