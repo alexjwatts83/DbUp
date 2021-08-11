a1dd3e553f3ecd7eb46c25863c7967a2c1d1e877
Alex Watts <alexjwatts83@gmail.com>
2021-08-11 18:38:11 +1000

    Little code clean up

--- src/DbUp.Api/Startup.cs
+++ src/DbUp.Api/Startup.cs
@@ -1,18 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using DbUp.Api.Extensions;
 using DbUp.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 namespace DbUp.Api
 {
